0ab840e3385f15d035602b1e601d8839c42e9b12
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-10-04 22:26:15 +0300

    Done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,7 @@
+abn
+
+
+
 import React, { useState } from "react";
 
 function Counter() {
@@ -5,8 +9,10 @@ function Counter() {
 
   function increment() {
     setCount((currentCount) => currentCount + 1);
-    setCount((currentCount) => currentCount + 1);
+    // setCount((currentCount) => currentCount + 1);
   }
 return <button onClick={increment}>I have been clicked {count} times</button>;
 }
 export default Counter;
+
+import React from 'react';
